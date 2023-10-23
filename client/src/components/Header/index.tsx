97a4d5402e6abe8769a719c8e0af8275f7445ed9
SealyBoi <aiden.mathews2002@gmail.com>
2023-10-23 15:57:03 -0500

    Reorganizing Some Code

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { PrimaryNav, MenuLink, Menu, Logo } from "./NavElement";
-import { HOME, ABOUT_US, CHARTS, TERMS } from "../../data/routes";
+import { HOME, ABOUT_US, CHARTS, TERMS, GAME } from "../../data/routes";
 
 const Navbar = () => {
   return (
@@ -8,6 +8,7 @@ const Navbar = () => {
         <Logo>The Gamblers Inc.</Logo>
         <Menu>
           <MenuLink to={HOME}>Home</MenuLink>
+          <MenuLink to={GAME}>Play the Game</MenuLink>
           <MenuLink to={ABOUT_US}>About Us</MenuLink>
           <MenuLink to={CHARTS}>Charts</MenuLink>
           <MenuLink to={TERMS}>Terms</MenuLink>
