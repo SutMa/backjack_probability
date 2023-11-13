e8cc6b7d8570e5f3f39593c5fa0859efe39e9f85
SealyBoi <aiden.mathews2002@gmail.com>
2023-11-13 13:47:12 -0600

    Fixed Home Page underline bug

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <PrimaryNav>
         <Logo>The Gamblers Inc.</Logo>
         <Menu>
-          <MenuLink to={HOME}>Home</MenuLink>
+          <MenuLink to={HOME} end>Home</MenuLink>
           <MenuLink to={GAME}>Play the Game</MenuLink>
           <MenuLink to={ABOUT_US}>About Us</MenuLink>
           <MenuLink to={CHARTS}>Charts</MenuLink>
